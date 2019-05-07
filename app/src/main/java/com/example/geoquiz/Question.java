b38fabb593833920e628124cf6270895686aa3fd
DidenkoBV <Didenko.B.V@sberbank.ru>
2019-05-07 15:40:52 +0300

    кнопка Next и переопределение слушателей на кнопках

--- app/src/main/java/com/example/geoquiz/Question.java
+++ app/src/main/java/com/example/geoquiz/Question.java
@@ -1,4 +1,29 @@
 package com.example.geoquiz;
 
 public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public Question(){}
+
+    public int getmTextResId() {
+        return mTextResId;
+    }
+
+    public void setmTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean ismAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setmAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
 }
